bd8494a8a0052e2e3b706badf16d47d5cbc83145
Henry Chan <henry.chan@mec.ca>
2019-05-08 11:22:25 -0700

    fixed typo

--- puzzle-creator/index.js
+++ puzzle-creator/index.js
@@ -1,7 +1,7 @@
 var indexTable = 'aec5yj18on20';
 var encDecoder = require('base-x')(indexTable)
 
-var plainText = "Congratulations!  You figured out this puzzle.  Unzip the encrypted zip file using the indexTable as the salt"
+var plainText = "Congratulations!  You figured out this puzzle.  Unzip the encoded zip file using the indexTable as the salt"
  
  
 console.log(encDecoder.encode(Buffer.from(plainText)))
