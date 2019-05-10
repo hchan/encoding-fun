7945eb45b9d13fae0e9caf957cbb081ee375f996
Henry Chan <henry.chan@mec.ca>
2019-05-10 11:36:00 -0700

    added reward.zip

--- puzzle-creator/index.js
+++ puzzle-creator/index.js
@@ -1,7 +1,13 @@
+const fs = require("fs");
+
 var indexTable = 'aec5yj18on20';
 var encDecoder = require('base-x')(indexTable)
 
-var plainText = "Congratulations!  You figured out this puzzle.  Unzip the encoded zip file using the indexTable as the salt"
+var plainText = "Congratulations!  You figured out this puzzle.  Unzip the encrypted zip file (reward.zip) using the indexTable as the password"
  
- 
-console.log(encDecoder.encode(Buffer.from(plainText)))
+var encoded = encDecoder.encode(Buffer.from(plainText));
+console.log(encoded)
+fs.writeFile("../puzzle-public/encoded.txt", encoded, (err) => {
+    if (err) console.log(err);
+    console.log("Successfully Written to File.");
+});
